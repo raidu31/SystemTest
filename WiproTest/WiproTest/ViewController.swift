b1745ca980024878ad1f75b9dcb36a34d3beee95
raidu31 <raidu31@gmail.com>
2020-07-02 20:01:07 +0530

    Add files via upload
    Added git to project

---
+++ WiproTest/WiproTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproTest
+//
+//  Created by Raidu on 7/2/20.
+//  Copyright © 2020 Raidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
